1d4cb4523a6f3a69cd4ab603f8f47ce80f54fa9e
Divya-Sree02 <pothugantidivyasree@gmail.com>
2026-03-06 19:16:32 +0530

    Modify tb_async_fifo testbench for extended cycles
    Increased the number of write and read cycles in the testbench and added conditional enabling for writing.

--- tb_async_fifo.v
+++ tb_async_fifo.v
@@ -1,4 +1,4 @@
- module tb_async_fifo;
+  module tb_async_fifo;
 
 reg wr_clk = 0;
 reg rd_clk = 0;
@@ -24,39 +24,42 @@ async_fifo DUT (
     .empty  (empty)
 );
 
-always #5  wr_clk = ~wr_clk;
-always #8  rd_clk = ~rd_clk;
+always #5 wr_clk = ~wr_clk;
+always #8 rd_clk = ~rd_clk;
 
 initial begin
     $dumpfile("fifo.vcd");
     $dumpvars(0, tb_async_fifo);
 
-    wr_en   = 0;
-    rd_en   = 0;
+    wr_en = 0;
+    rd_en = 0;
     wr_data = 0;
 
     #20 reset = 0;
-
-    repeat (10) begin
+ 
+    repeat (20) begin
         @(posedge wr_clk);
         if (!full) begin
-            wr_en   = 1;
-            wr_data = wr_data + 8'h01;
+            wr_en = 1;
+            wr_data = wr_data + 1;
+        end
+        else begin
+            wr_en = 0;
         end
     end
-    wr_en = 0;
 
     #50;
 
-    repeat (10) begin
+    repeat (20) begin
         @(posedge rd_clk);
         if (!empty)
             rd_en = 1;
+        else
+            rd_en = 0;
     end
-    rd_en = 0;
 
-    #100;
-    $finish;
+    #100 $finish;
+
 end
 
 endmodule
